7f26bca2ad168a779f29a9f88f9eca82d1edc6c9
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2024-01-25 12:56:55 +0530

    Update HomeScreen.dart

--- HomeScreen.dart
+++ HomeScreen.dart
@@ -9,8 +9,8 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-}
-
+} 
+ 
 class _HomeScreenState extends State<HomeScreen> {
   bool Personal = true, College = false, Office = false;
 
